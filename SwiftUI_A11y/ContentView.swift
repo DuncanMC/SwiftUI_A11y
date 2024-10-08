a6002d93449f12ff5552a58ae497304a23b18b9d
Duncan Champney <DChampney@WareTo.com>
2024-10-08 14:31:56 -0400

    Initial Commit

---
+++ SwiftUI_A11y/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_A11y
+//
+//  Created by Duncan Champney on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_A11y/SwiftUI_A11yApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_A11yApp.swift
+//  SwiftUI_A11y
+//
+//  Created by Duncan Champney on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_A11yApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
